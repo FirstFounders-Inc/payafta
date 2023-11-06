a57446287ba3ac55607ee457fe822b15eca21efd
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-11-06 16:53:34 +0100

    chore: implemted the navbar of the home page

---
+++ src/modules/authentication/Home/components/Navbar.tsx
@@ -0,0 +1,13 @@
+// import { ReactComponent as Logo } from '@assets/svg/Logo.svg';
+const Navbar = () => {
+  return (
+    <div className="flex justify-between">
+      <div>
+        {/* <Logo /> */}
+      </div>
+      <div>b</div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/modules/authentication/Home/index.tsx
+++ src/modules/authentication/Home/index.tsx
@@ -1,5 +1,11 @@
+import Navbar from './components/Navbar';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="px-[48px]">
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
