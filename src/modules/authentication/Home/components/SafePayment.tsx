96f497dbc71f9b8d072e93e54e11cd42405d8834
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-11-07 17:31:29 +0100

    chore: implemented howItWorks component

--- src/modules/authentication/Home/components/SafePayment.tsx
+++ src/modules/authentication/Home/components/SafePayment.tsx
@@ -13,7 +13,7 @@ const safePaymentObj = [
 
 const SafePayment = () => {
   return (
-    <div className="flex flex-col md:flex-row gap-2  mt-[68px] md:mt-[86px]">
+    <div className="flex flex-col lg:flex-row gap-2  pt-[38px] md:pt-[66px] pb-[30px] lg:pb-[54px] ">
       <div className="md:flex-1">
         {safePaymentObj?.map((el, idx) => {
           return (
@@ -21,13 +21,13 @@ const SafePayment = () => {
               <p className=" rounded-[16px] md:rounded-[24px] bg-[#86C8E9] p-2 w-[272px] text-[#fff] uppercase  text-[14px] font-600">
                 {el?.smallText}
               </p>
-              <h1 className=" text-[52px] md:text-[80px] font-normal text-[#03045B] w-[351px] md:w-[539px]  leading-[65px] md:leading-[100px] uppercase mb-[10px]">
+              <h1 className=" text-[52px] lg:text-[80px] font-normal text-[#03045B] w-[351px]  md:w-[700px] lg:w-[539px]  leading-[65px] md:leading-[100px] uppercase mb-[10px]">
                 {el?.bigText}
               </h1>
               <p className="text-[#333333] text-[16px] md:text-[20px] leading-[20px] md:leading-[25px] font-medium w-[350px] md:w-[649px] mb-[24px]">
                 {el?.paragraph}
               </p>
-              <button className="flex gap-2  rounded-[31px] md:rounded-[48px] bg-[#03045B]  p-4 md:p-6">
+              <button className="flex gap-2  rounded-[31px] lg:rounded-[48px] bg-[#03045B]  p-4 lg:p-6">
                 <p className="text-[#fff]">{el?.button}</p>
                 <img src={rightAccordion} alt="an accordion" />
               </button>
@@ -35,7 +35,7 @@ const SafePayment = () => {
           );
         })}
       </div>
-      <div className="mt-16 md:mt-0">
+      <div className="mt-16 lg:mt-0">
         <img src={desktop} alt="desktop screen" />
       </div>
     </div>
--- src/modules/authentication/Home/index.tsx
+++ src/modules/authentication/Home/index.tsx
@@ -1,11 +1,13 @@
+import HowItWorks from './components/HowItWorks';
 import Navbar from './components/Navbar';
-import SafePayment from './components/SafePayment';
+// import SafePayment from './components/SafePayment';
 
 const Home = () => {
   return (
-    <div className="px-[24px] md:px-[48px] mt-[24px]">
+    <div className="px-[24px] lg:px-[48px] mt-[24px]">
       <Navbar />
-      <SafePayment />
+      {/* <SafePayment /> */}
+      <HowItWorks />
     </div>
   );
 };
