68a2da14afe03e45966a8ffe38be2c83e4bfdc5b
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-11-07 10:25:48 +0100

    chore: implemented navbar and mobile screen

--- src/modules/authentication/Home/index.tsx
+++ src/modules/authentication/Home/index.tsx
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar';
 
 const Home = () => {
   return (
-    <div className="px-[48px]">
+    <div className="px-[24px] md:px-[48px] mt-[24px]">
       <Navbar />
     </div>
   );
