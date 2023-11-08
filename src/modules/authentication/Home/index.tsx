93f31dc0606b2bb229e1b23e6a674aa9169dd460
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-11-08 11:50:09 +0100

    chore: implemented footer component

--- src/modules/authentication/Home/index.tsx
+++ src/modules/authentication/Home/index.tsx
@@ -6,6 +6,7 @@ import SellerProtection from './components/SellerProtection';
 import SafePayment from './components/SafePayment';
 import SetUpEscrow from './components/SetUpEscrow';
 import Faq from './components/Faq';
+import Footer from './components/Footer';
 
 const Home = () => {
   return (
@@ -18,6 +19,7 @@ const Home = () => {
       <WhatCanYouBuy />
       <SetUpEscrow />
       <Faq />
+      <Footer />
     </div>
   );
 };
