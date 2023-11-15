97c3675cc6e976c843cc41d620695f5a6c637480
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-11-15 16:15:23 +0100

    chore: implemented create password component

--- src/modules/authentication/validation/authValidation.ts
+++ src/modules/authentication/validation/authValidation.ts
@@ -27,3 +27,18 @@ export const signupSchema = yup.object({
 export const forgotPasswordSchema = yup.object({
   email: yup.string().required('Email is required'),
 });
+
+export const createNewPasswordSchema = yup.object({
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(8, 'Password must be at least 8 character long')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}[\]:;<>,.?~\\-]).{8,}$/,
+      'Password must contain at least one uppercase letter, one lowercase letter, one special character',
+    ),
+  confirmpassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Passwords do not match')
+    .required('Required'),
+});
